dc387d9387afdf18edf7c720ae22452cd64795a4
2743-123 <2743-123>
2024-10-28 23:43:29 +0530

    issue

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,15 @@ import superadminRoutes from './routes/superadminRoutes'
 import { authMiddleware } from './middlewares/authMiddleware';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 
 const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')))
+
 
 app.use(express.json()); // Middleware to parse JSON
 app.use(bodyParser.json());
