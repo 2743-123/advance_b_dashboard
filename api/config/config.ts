05dfee6ccc94b0d064ec780e4ca63c224684d51e
2743-123 <2743-123>
2024-10-29 22:51:33 +0530

    fix bug

---
+++ api/config/config.ts
@@ -0,0 +1,20 @@
+require('dotenv').config(); // Add this line to load environment variables
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.DB_PORT
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.DB_PORT
+  }
+};
